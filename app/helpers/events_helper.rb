25160f2155bca9f91589d97a84f93809ce478f71
Ralph <ralphoburu@gmail.com>
2021-03-15 09:16:10 +0300

    Fix rubocop errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,14 +8,14 @@ module EventsHelper
   end
 
   def nav_links
-    if current_user 
+    if current_user
       render '/events/sign_in'
     else
       render '/events/sign_out'
     end
   end
 
-  def errors 
+  def errors
     render '/events/errors' if @event.errors.any?
   end
 end
